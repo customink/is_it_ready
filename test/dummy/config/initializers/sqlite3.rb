463c6d1d62eca04469e5e729afa85fffb087b491
Arne De Herdt <arne.deherdt@customink.com>
2022-09-16 09:39:35 +0200

    [Ruby on Rails] Wrap the SQLite configuration for supported versions

--- test/dummy/config/initializers/sqlite3.rb
+++ test/dummy/config/initializers/sqlite3.rb
@@ -5,4 +5,7 @@
 
 # Enforce booleans to represented as integers in the database.
 # This was an old SQLite feature that we do not want to support anymore.
-::Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+# Note: This is only for older versions of Sqlite3, the configuration option was dropped at some point
+if ::Rails.application.config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
+  ::Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+end
